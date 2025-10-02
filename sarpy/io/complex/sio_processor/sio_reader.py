204931ceb1fb3480d3267e2122d22406573d18a8
Peterson <529965@bah.com>
2025-10-02 18:27:09 -0400

    Updated SIO reader and writer.

--- sarpy/io/complex/sio_processor/sio_reader.py
+++ sarpy/io/complex/sio_processor/sio_reader.py
@@ -1,20 +1,187 @@
-# SIOReader is a class implementing a specific FlatfileImageReader for SICD
-# files using an SIO "carrier".  The SICD metadata is written in a user
-# data field called "SICDMETA" in the standard SIO file format.  
-#
-# Written by: Tex Peterson
+# SIOWriter is a class for writing SICD files using an SIO format.
+# The SICD metadata is written in a user data field called "SICDMETA" in the 
+# standard SIO file format.
+# 
+# Written by: Tex Peterson, 
+# Written on: 2025-10
 #
 # //////////////////////////////////////////
 # /// CLASSIFICATION: UNCLASSIFIED       ///
 # //////////////////////////////////////////
 
-# from sarpy.io.complex.generic.FlatfileImageReader import FlatfileImageReader
-# from sarpy.io.complex.sicd_elements.SICD import SICDType
+from xml.etree import ElementTree
+import numpy
+import os
+import struct
 
-# class SIOReader(FlatfileImageReader):
+from sarpy.io.complex.sicd_elements.SICD import SICDType
+from sarpy.io.general.base import SarpyIOError
+from sarpy.io.complex.sicd_elements.SICD import _SICD_SPEC_DETAILS, _SICD_VERSION_DEFAULT
+from sarpy.io.complex.sicd_elements.ImageData import ImageDataType, FullImageType
+from sarpy.io.complex.sicd_elements.blocks import RowColType
 
-#     def __init__(
-#         self,
-#         param_filename:              str, 
-#     ):
-#         super().__init__(param_filename=param_filename)
+class SIOReader(object):
+    
+    def __init__(
+        self,
+        param_filename
+    ):
+        self._filename =  param_filename
+        if not os.path.exists(os.path.dirname(self._filename)):
+            raise SarpyIOError('Path {} is not a file'.format(self._filename))
+        self._fid = open(self._filename, 'rb') # We always read big-endian
+        self._magic_key      = int.from_bytes(self._fid.read(4))
+        self._rows           = int.from_bytes(self._fid.read(4))
+        self._columns        = int.from_bytes(self._fid.read(4))
+        self._data_type_code = int.from_bytes(self._fid.read(4))
+        self.sio_code_to_numpy_data_type()
+        self._data_size      = int.from_bytes(self._fid.read(4))
+        match self._magic_key: ## Problem with case matching. hex() returns a string, leave as hex
+            case 0xFF017FFE:
+                # SIO file written as big endian without user data
+                print('SIO file written as big endian without user data')
+                self._user_data   = None
+                self._image_data  = numpy.frombuffer(self._fid.read(), 
+                                                     dtype=self._data_type_str).reshape(self._rows, self._columns,2)
+            case 0xFE7F01FF:
+                # SIO file written as little endian without user data
+                print('SIO file written as big endian without user data')
+                self._user_data   = None
+                temp_image_data   = self._fid.read()
+                # Reverse the bytes ingested from little endian to big
+                self._image_data  = numpy.frombuffer(temp_image_data[::-1], 
+                                                     dtype=self._data_type_str).reshape(self._rows, self._columns,2)
+            case 0xFF027FFD: 
+                # SIO file written as big endian with user data (sicd meta)
+                print('SIO file written as big endian with user data')
+                # Num pairs   (4 byte uint, # pairs of user data, fixed at 1)
+                # Name bytes  (4 byte uint, # bytes in name of user element,
+                #             fixed at 8 for name "SICDMETA")
+                # Name        (8 bytes containing "SICDMETA")
+                # Value bytes (4 byte uint, value is length of XML string)
+                # Value       (XML string holding SICD metadata)
+                self._num_pairs = int.from_bytes(self._fid.read(4))
+                user_data_name_bytes = int.from_bytes(self._fid.read(4))
+                self._user_data_name = self._fid.read(user_data_name_bytes).decode("utf-8")
+                self._user_data_size  = int.from_bytes(self._fid.read(4))
+                self._user_data    = self._fid.read(self._user_data_size).decode("utf-8")
+                self._user_data_xml = ElementTree.fromstring(self._user_data)
+                self.sio_user_data_to_sarpy_format()
+                self._image_data  = numpy.frombuffer(self._fid.read(), 
+                                                     dtype=self._data_type_str).reshape(self._rows, self._columns,2)
+            case 0xFD7F02FF:
+                # SIO file written as little endian with user data (sicd meta)
+                print('SIO file written as little endian with user data')
+                # Num pairs   (4 byte uint, # pairs of user data, fixed at 1)
+                # Name bytes  (4 byte uint, # bytes in name of user element,
+                #             fixed at 8 for name "SICDMETA")
+                # Name        (8 bytes containing "SICDMETA")
+                # Value bytes (4 byte uint, value is length of XML string)
+                # Value       (XML string holding SICD metadata)
+                self._num_pairs = int.from_bytes(self._fid.read(4))
+                user_data_name_bytes = int.from_bytes(self._fid.read(4))
+                self._user_data_name = self._fid.read(user_data_name_bytes).decode("utf-8")
+                self._user_data_size  = int.from_bytes(self._fid.read(4))
+                self._user_data    = self._fid.read(self._user_data_size).decode("utf-8")
+                self._user_data_xml = ElementTree.fromstring(self._user_data)
+                self.sio_user_data_to_sarpy_format()
+                temp_image_data   = self._fid.read()
+                # Reverse the bytes ingested from little endian to big
+                self._image_data  = numpy.frombuffer(temp_image_data[::-1], 
+                                                     dtype=self._data_type_str).reshape(self._rows, self._columns,2)
+
+    def sio_code_to_numpy_data_type(self):
+        match self._data_type_code:
+            case 1:
+                self._data_type_str = 'int16'
+                self._data_size = 2
+            case 3:
+                self._data_type_str = 'float32'
+                self._data_size = 8
+            case 13:
+                self._data_type_str = 'complex64'
+                self._data_size = 16
+            case _ : #Default if other cases don't match
+                raise TypeError('Writer only recognizes floats, complex and signed or unsigned integers')
+            
+    def sio_user_data_to_sarpy_format(self):
+        if self._user_data_xml.tag[-4:] == 'SICD':
+            self.sio_user_data_to_sicd_format()
+        elif self._user_data_xml.tag[-4:] == 'CPHD':
+            self.sio_user_data_to_cphd_format()
+
+    def sio_user_data_to_sicd_format(self):
+        # Initialize variables that will be set later.
+        meta_data_number = 0
+        NumRows = 0
+        NumCols=0
+        PixelType=0
+        FirstRow=0
+        FirstCol=0
+        NumRows=0
+        NumCols=0
+        FullImageNumRows = 0 
+        FullImageNumCols = 0
+        SCPPixelRow=0
+        SCPPixelCol=0
+        FullImage = False
+        for elem in self._user_data_xml.iter():
+            if elem.tag[-4:] == 'SICD':
+                print(_SICD_SPEC_DETAILS[_SICD_VERSION_DEFAULT]['namespace'] + '}SICD')
+                if meta_data_number > 0: # We only process the first SICD meta data
+                    break
+                else:
+                    meta_data_number += 1
+            elif elem.tag[-9:] == 'ImageData':
+                print(_SICD_SPEC_DETAILS[_SICD_VERSION_DEFAULT]['namespace'] + '}ImageData')
+            elif elem.tag[-9:] == 'PixelType':
+                PixelType = elem.text
+            elif elem.tag[-7:] == 'NumRows':
+                if FullImage:
+                    FullImageNumRows = elem.text
+                else:
+                    NumRows = elem.text
+            elif elem.tag[-7:] == 'NumCols':
+                if FullImage:
+                    FullImageNumCols = elem.text
+                    FullImage = False
+                else:
+                    NumCols = elem.text
+            elif elem.tag[-8:] == 'FirstRow':
+                FirstRow = elem.text
+            elif elem.tag[-8:] == 'FirstCol':
+                FirstCol = elem.text
+            elif elem.tag[-9:] == 'FullImage':
+                FullImage = True
+            elif elem.tag[-8:] == 'SCPPixel':
+                SCPPixel = True
+            elif elem.tag[-3:] == 'Row':
+                if SCPPixel:
+                    SCPPixelRow = elem.text
+            elif elem.tag[-3:] == 'Col':
+                if SCPPixel:
+                    SCPPixelCol = int(elem.text)
+            else:
+                print('break: ' + elem.tag)
+        localImageData=ImageDataType(
+        	NumRows=NumRows,
+            NumCols=NumCols,
+            PixelType=PixelType,
+            FirstRow=FirstRow,
+            FirstCol=FirstCol,
+            FullImage=FullImageType(
+                NumRows=FullImageNumRows,
+                NumCols=FullImageNumCols
+            ),
+            SCPPixel=RowColType(Row=SCPPixelRow , Col=SCPPixelCol)
+        )
+        self._sicdmeta = SICDType(ImageData=localImageData)
+        
+    def sio_user_data_to_cphd_format(self):
+        raise NotImplementedError
+                
+
+
+# //////////////////////////////////////////
+# /// CLASSIFICATION: UNCLASSIFIED       ///
+# //////////////////////////////////////////
--- sarpy/io/complex/sio_processor/sio_writer.py
+++ sarpy/io/complex/sio_processor/sio_writer.py
@@ -1,38 +1,74 @@
-# SIOWriter is a class implementing a specific FlatfileImageWriter for SICD
-# files using an SIO "carrier".  The SICD metadata is written in a user
-# data field called "SICDMETA" in the standard SIO file format.  The caller
-# can also turn off the user data, since not all SIO readers handle this.
+# SIOWriter is a class for writing SICD files using an SIO format.
+# The SICD metadata is written in a user data field called "SICDMETA" in the 
+# standard SIO file format. The caller can also turn off the user data, since 
+# not all SIO readers handle this.
 #
 # Written by: Tex Peterson
 # Written on: 2025-10
-# Derrived from MATLAB_SAR 
 #
 # //////////////////////////////////////////
 # /// CLASSIFICATION: UNCLASSIFIED       ///
 # //////////////////////////////////////////
 
-from sarpy.io.complex.generic.FlatfileImageWriter import FlatfileImageWriter
-from sarpy.io.complex.sicd_elements.SICD import SICDType
+import numpy
+import os
 
-class SIOWriter(FlatfileImageWriter):
+from sarpy.io.complex.sicd_elements.SICD import SICDType
+from sarpy.io.general.base import SarpyIOError
+
+class SIOWriter(object):
     
     def __init__(
-            # Parse inputs
-            # These inputs are really parsed later in the superclass
-            # constructor.  Here just need to 1) extract
-            # include_sicd_metadata, which is specific to SIOWriter class
-            # and 2) make sure that no additional arguments are passed--
-            # like header_skip, which could break things, if passed on to
-            # the superclass constructor.
             self,
             param_filename:              str, 
-            param_sicdmeta:              SICDType,
-            # numpy data types. "uint8", "float32", etc.
-            param_data_type_str:         str = 'complex64', 
-            param_data_type_code:        int = 13,
-            param_is_complex:            bool = True,       # Boolean
-            param_include_sicd_metadata: bool = True,       # Boolean
+            param_image_data:            numpy.array, 
+            param_sicdmeta:              SICDType|None = None,
+            param_start_indices:         list = [0, 0],
+            param_include_sicd_metadata: bool = True       # Boolean
     ):
+        # Parse inputs
+        self._filename       = param_filename
+        self._image_data     = param_image_data
+        if param_sicdmeta is not None:
+            self._sicdmeta   = param_sicdmeta.to_xml_bytes()
+        self._start_indices  = param_start_indices
+        self._include_sicd_metadata = param_include_sicd_metadata
+        
+        self._header_skip = 5*4 # SIO header with no user data is 5 uint32 words
+
+        # Magic key
+        # We always write big-endian
+        if self._sicdmeta is not None and self._include_sicd_metadata:
+            self._header_skip = self._header_skip + 4 + 4 + 8 + 4 + \
+                len(self._sicdmeta) # Add user data length
+            self._magic_key = 0xFF027FFD # Indicates big endian, with user-data
+        else:
+            self._magic_key = 0xFF017FFE # Indicates big endian, with no user-data
+        self._image_shape    = self._image_data.shape
+        
+        # Data type and size
+        self.numpy_data_type_to_sio()
+        
+        if not os.path.exists(os.path.dirname(self._filename)):
+            raise SarpyIOError('Path {} is not a file'.format(self._filename))
+        self._fid = open(self._filename, 'wb')
+        
+    
+    def numpy_data_type_to_sio(self):
+        match self._image_data.dtype:
+            case 'int16':
+                self._data_type_code = 1
+                self._data_size = 2
+            case 'float32':
+                self._data_type_code = 3
+                self._data_size = 8
+            case 'complex64':
+                self._data_type_code = 13
+                self._data_size = 16
+            case _ : #Default if other cases don't match
+                raise TypeError('Writer only recognizes floats, complex and signed or unsigned integers')
+                
+    def write(self):
         # We'll use a fixed definition SIO header with (possibly) one user-data segment.
         # The header will the look like this:
         #    Core SIO header:
@@ -48,58 +84,28 @@ class SIOWriter(FlatfileImageWriter):
         #       Name        (8 bytes containing "SICDMETA")
         #       Value bytes (4 byte uint, value is length of XML string)
         #       Value       (XML string holding SICD metadata)
-        self._include_sicd_metadata = param_include_sicd_metadata
-        header_skip = 5*4 # SIO header with no user data is 5 uint32 words
-        if self._include_sicd_metadata:
-            XML_meta_string = param_sicdmeta.to_xml_string()
-            header_skip = header_skip + 4 + 4 + 8 + 4 + len(XML_meta_string); # Add user data length
         
-        # Call the init function of the parent class
-        super().__init__(param_filename=param_filename, 
-                         param_sicdmeta=param_sicdmeta,
-                         param_data_type_str=param_data_type_str,
-                         param_data_type_code=param_data_type_code,
-                         param_is_complex=param_is_complex,
-                         param_header_skip=header_skip)
-        
-        # Magic key
-        # We always write big-endian
-        if param_include_sicd_metadata:
-            magic_key = 0xFF027FFD # Indicates big endian, with user-data
-        else:
-            magic_key = 0xFF017FFE # Indicates big endian, with no user-data
-        self._fid.seek(0)
-        self._fid.write(magic_key.to_bytes(4))
+
+        self._fid.write(self._magic_key.to_bytes(4))
         # Rows and columns
-        self._fid.write(self._image_size[1].to_bytes(4))
-        self._fid.write(self._image_size[0].to_bytes(4))
-        # Data type and size
-        self.numpy_data_type_to_sio()
+        self._fid.write(self._image_shape[0].to_bytes(4))
+        self._fid.write(self._image_shape[1].to_bytes(4))
         self._fid.write(self._data_type_code.to_bytes(4))
         self._fid.write(self._data_size.to_bytes(4))
         # User data
-        if self._include_sicd_metadata:
-            self._fid.write((1).to_bytes(4))                    # Num pairs of user data
-            self._fid.write((8).to_bytes(4))                    # Pair 1 name length
-            self._fid.write('SICDMETA'.encode('utf-8'))           # Pair 1 name
-            self._fid.write(len(XML_meta_string).to_bytes(4)) # Pair 1 value length
-            self._fid.write(XML_meta_string.encode('utf-8'))      # Pair 1 value
+        if self._sicdmeta is not None and self._include_sicd_metadata:
+            # Num pairs of user data, always 1 because we only write 1 SICD at a time.
+            self._fid.write((1).to_bytes(4))                  
+            self._fid.write((8).to_bytes(4))                 # Name length
+            self._fid.write('SICDMETA'.encode('utf-8'))      # Always SICDMEATA
+            self._fid.write(len(self._sicdmeta).to_bytes(4)) 
+            self._fid.write(self._sicdmeta.encode('utf-8'))  
+
+        num_bytes_written = self._fid.write(self._image_data)
+        return num_bytes_written
     
-    def numpy_data_type_to_sio(self):
-        match self._data_type_str:
-            case 'int16':
-                self._data_type_code = 1
-                self._data_size = 2
-            case 'float32':
-                self._data_type_code = 3
-                self._data_size = 8
-            case 'complex64':
-                self._data_type_code = 13
-                self._data_size = 16
-            case _ : #Default if other cases don't match
-                raise TypeError('Writer only recognizes floats, complex and signed or unsigned integers')
-                
-    
+    def close(self):
+        self._fid.close()
 
 # //////////////////////////////////////////
 # /// CLASSIFICATION: UNCLASSIFIED       ///
