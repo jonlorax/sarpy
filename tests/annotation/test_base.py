75d7a7aa6b8af94884d9c5ebf263bbd041f470c0
Hallie <hallie.khuong@gmail.com>
2025-11-12 20:41:37 +0000

    rewrite tests using class and self.assertEquals format and started writing tests for annotationproperties

--- tests/annotation/test_base.py
+++ tests/annotation/test_base.py
@@ -1,123 +1,198 @@
 from collections import OrderedDict
 import pytest
 import re
-from uuid import UUID
+import unittest
+from uuid import UUID, uuid4
 
-from sarpy.annotation.base import GeometryProperties
+from sarpy.annotation.base import GeometryProperties, AnnotationProperties, AnnotationFeature, AnnotationCollection, FileAnnotationCollection
+from sarpy.geometry.geometry_elements import Jsonable
 
-def test_geometryproperties_default_uid():
-    # confirms that a uuid is generated if one isn't provided
-    geom = GeometryProperties()
-    assert isinstance(geom.uid, str)
+class test_geometryproperties(unittest.TestCase):
+    def test_geometryproperties_default_uid(self):
+        # confirms that a uuid is generated if one isn't provided
+        geom = GeometryProperties()
+        self.assertIsInstance(geom.uid, str)
 
-    try:
-        UUID(geom.uid)
-    except:
-        pytest.fail("The generated uid is not a valid UUID string")
+        try:
+            UUID(geom.uid)
+        except:
+            pytest.fail("The generated uid is not a valid UUID string")
 
-def test_geometryproperties_custom_uid():
-    # confirms that that the uid setter works as expected
-    custom_uid = "abcd"
-    geom = GeometryProperties(uid=custom_uid)
-    assert geom.uid == custom_uid
-
-def test_geometryproperties_invalid_uid():
-    custom_uid = 1234
-    with pytest.raises(TypeError, 
-                        match = re.escape("uid must be a string")):
+    def test_geometryproperties_custom_uid(self):
+        # confirms that that the uid setter works as expected
+        custom_uid = "abcd"
         geom = GeometryProperties(uid=custom_uid)
+        self.assertEquals(geom.uid, custom_uid)
 
-def test_geometryproperties_default_name():
-    geom = GeometryProperties()
-    assert geom.name == None
+    def test_geometryproperties_invalid_uid(self):
+        custom_uid = 1234
+        with pytest.raises(TypeError, 
+                            match = re.escape("uid must be a string")):
+            geom = GeometryProperties(uid=custom_uid)
 
-def test_geometryproperties_custom_name():
-    custom_name = "abcd"
-    geom = GeometryProperties(name=custom_name)
-    assert geom.name == custom_name
+    def test_geometryproperties_default_name(self):
+        geom = GeometryProperties()
+        self.assertIsNone(geom.name)
 
-def test_geometryproperties_invalid_name():
-    custom_name = 1234
-    with pytest.raises(TypeError, 
-                        match = re.escape("Got unexpected type for name")):
+    def test_geometryproperties_custom_name(self):
+        custom_name = "abcd"
         geom = GeometryProperties(name=custom_name)
+        self.assertEquals(geom.name, custom_name)
 
-def test_geometryproperties_default_color():
-    geom = GeometryProperties()
-    assert geom.color == None
+    def test_geometryproperties_invalid_name(self):
+        custom_name = 1234
+        with pytest.raises(TypeError, 
+                            match = re.escape("Got unexpected type for name")):
+            geom = GeometryProperties(name=custom_name)
 
-def test_geometryproperties_custom_color():
-    custom_color = "abcd"
-    geom = GeometryProperties(color=custom_color)
-    assert geom.color == custom_color
+    def test_geometryproperties_default_color(self):
+        geom = GeometryProperties()
+        self.assertIsNone(geom.color)
 
-def test_geometryproperties_invalid_color():
-    custom_color = 1234
-    with pytest.raises(TypeError, 
-                        match = re.escape("Got unexpected type for color")):
+    def test_geometryproperties_custom_color(self):
+        custom_color = "abcd"
         geom = GeometryProperties(color=custom_color)
+        self.assertEquals(geom.color, custom_color)
 
-def test_geometryproperties_all_fields():
-    # makes sure that GeomeryProperties is able to be initialized properly with all three parameters passed
-    geom = GeometryProperties(uid="abcd", name="efgh", color="blue")
+    def test_geometryproperties_invalid_color(self):
+        custom_color = 1234
+        with pytest.raises(TypeError, 
+                            match = re.escape("Got unexpected type for color")):
+            geom = GeometryProperties(color=custom_color)
 
-    assert geom.uid == "abcd"
-    assert geom.name == "efgh"
-    assert geom.color == "blue"
+    def test_geometryproperties_all_fields(self):
+        # makes sure that GeomeryProperties is able to be initialized properly with all three parameters passed
+        geom = GeometryProperties(uid="abcd", name="efgh", color="blue")
 
-def test_from_dict_valid_data():
-    data = {
-        "type": GeometryProperties._type,
-        "uid": "abcd",
-        "name": "efgh",
-        "color": "red"
-    }
+        self.assertEquals(geom.uid, "abcd")
+        self.assertEquals(geom.name, "efgh")
+        self.assertEquals(geom.color, "blue")
 
-    geom = GeometryProperties.from_dict(data)
+    def test_from_dict_valid_data(self):
+        data = {
+            "type": GeometryProperties._type,
+            "uid": "abcd",
+            "name": "efgh",
+            "color": "red"
+        }
 
-    assert isinstance(geom, GeometryProperties)
-    assert geom.uid == "abcd"
-    assert geom.name == "efgh"
-    assert geom.color == "red"
+        geom = GeometryProperties.from_dict(data)
 
-def test_from_dict_missing_type():
-    # confirms that an error is raised if the dict is missing type
-    bad_data = {
-        "uid": "abcd",
-        "name": "efgh",
-        "color": "red"
-    }
+        self.assertIsInstance(geom, GeometryProperties)
+        self.assertEquals(geom.uid, "abcd")
+        self.assertEquals(geom.name, "efgh")
+        self.assertEquals(geom.color, "red")
 
-    # with pytest.raises(ValueError, 
-    #                     match = re.escape("Geometry Properties cannot be constructed from")):
-    geom = GeometryProperties.from_dict(bad_data)
-    # key error is currently being thrown? not sure if it should be the value error as stated in the from_dict function
+    def test_from_dict_missing_type(self):
+        # confirms that an error is raised if the dict is missing type
+        bad_data = {
+            "uid": "abcd",
+            "name": "efgh",
+            "color": "red"
+        }
 
-def test_to_dict_geometryproperties_w_values():
-    # confirms that the to_dict() function works as expected and includes all of the attributes in a dictionary 
-    geom_dict = GeometryProperties(uid="abcd", name="efgh", color="blue").to_dict()
-
-    assert isinstance(geom_dict, dict)
-    assert geom_dict["uid"] == "abcd"
-    assert geom_dict["name"] == "efgh"
-    assert geom_dict["color"] == "blue"
-
-def test_to_dict_geometryproperties_exclude_none_fields():
-    # confirms that None fields are excluded
-    geom_dict = GeometryProperties(uid="abcd", name=None, color=None).to_dict()
+        with pytest.raises(KeyError,
+                            match = re.escape("the json requires the field 'type'")):
+            geom = GeometryProperties.from_dict(bad_data)
     
-    assert isinstance(geom_dict, dict)
-    assert geom_dict["uid"] == "abcd"
-    assert "name" not in geom_dict
-    assert "color" not in geom_dict
+    def test_from_dict_missing_type(self):
+        # confirms that an error is raised if the dict is missing type
+        bad_data = {
+            "type": "string",
+            "uid": "abcd",
+            "name": "efgh",
+            "color": "red"
+        }
 
-def test_to_dict_w_parent_dict():
-    # confirms that the to_dict() function works with a parent_dict passed
-    parent_dict = OrderedDict([("key", "value")])
+        with pytest.raises(ValueError, 
+                            match = re.escape("GeometryProperties cannot be constructed from {'type': 'string', 'uid': 'abcd', 'name': 'efgh', 'color': 'red'}")):
+            geom = GeometryProperties.from_dict(bad_data)
 
-    geom_dict = GeometryProperties(uid="abcd", name="efgh", color="blue").to_dict(parent_dict)
+    def test_to_dict_geometryproperties_w_values(self):
+        # confirms that the to_dict() function works as expected and includes all of the attributes in a dictionary 
+        geom_dict = GeometryProperties(uid="abcd", name="efgh", color="blue").to_dict()
 
-    assert geom_dict["key"] == "value"
-    assert geom_dict["uid"] == "abcd"
-    assert geom_dict["name"] == "efgh"
-    assert geom_dict["color"] == "blue"
+        self.assertIsInstance(geom_dict, dict)
+        self.assertEquals(geom_dict["uid"], "abcd")
+        self.assertEquals(geom_dict["name"], "efgh")
+        self.assertEquals(geom_dict["color"], "blue")
+
+    def test_to_dict_geometryproperties_exclude_none_fields(self):
+        # confirms that None fields are excluded
+        geom_dict = GeometryProperties(uid="abcd", name=None, color=None).to_dict()
+        
+        self.assertIsInstance(geom_dict, dict)
+        self.assertEquals(geom_dict["uid"], "abcd")
+        self.assertNotIn("name", geom_dict)
+        self.assertNotIn("color", geom_dict)
+
+    def test_to_dict_w_parent_dict(self):
+        # confirms that the to_dict() function works with a parent_dict passed
+        parent_dict = OrderedDict([("key", "value")])
+
+        geom_dict = GeometryProperties(uid="abcd", name="efgh", color="blue").to_dict(parent_dict)
+
+        self.assertEquals(geom_dict["key"], "value")
+        self.assertEquals(geom_dict["uid"], "abcd")
+        self.assertEquals(geom_dict["name"], "efgh")
+        self.assertEquals(geom_dict["color"], "blue")
+
+@pytest.fixture
+def geometry_data():
+    # return {"type": "GeometryProperties",
+    #         "uid": str(uuid4()),
+    #         "name": "apple",
+    #         "color": "red"}
+    return {
+            "type": GeometryProperties._type,
+            "uid": "abcd",
+            "name": "efgh",
+            "color": "red"
+        }
+    
+@pytest.fixture
+def geometryproperties_obj():
+    return GeometryProperties.from_dict(geometry_data)
+
+class test_annotationproperties(unittest.TestCase):
+    def test_annotationproperties_default_initialization(self):
+        obj = AnnotationProperties()
+
+        self.assertIsNone(obj.name)
+        self.assertIsNone(obj.description)
+        self.assertIsNone(obj.directory)
+        self.assertEquals(obj.geometry_properties, [])
+        self.assertIsNone(obj.parameters)
+
+    def test_annotationproperties_initialization_w_values(self):
+        obj = AnnotationProperties(
+            name = "annotation1",
+            description = "abcd",
+            directory = "path/folder",
+            geometry_properties = [geometryproperties_obj],
+            parameters = Jsonable()
+        )
+
+        self.assertEquals(obj.name, "annotation1")
+        self.assertEquals(obj.description, "abcd")
+        self.assertEquals(obj.directory, "path/folder")
+        self.assertIsInstance(obj.geometry_properties, GeometryProperties)
+        self.assertIsInstance(obj.parameters, Jsonable)
+
+    def test_from_dict_valid_input(self):
+        data = {
+            "type": "AnnotationProperties",
+            "name": "annotation1",
+            "description": "abcd",
+            "directory": "path/folder",
+            "geometry_properties": [geometry_data],
+            "parameters": None
+        }
+        
+        obj = AnnotationProperties.from_dict(data)
+
+        self.assertIsInstance(obj, AnnotationProperties)
+        self.assertEquals(obj.name, "annotation1")
+        self.assertEquals(obj.description, "abcd")
+        self.assertEquals(obj.directory, "path/folder")
+        self.assertEquals(len(obj.geometry_properties), 1)
